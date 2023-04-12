25e9f0f5aff8ca9ed815d029c426861db9c7cb58
Victor <vict02r05@gmail.com>
2023-04-12 20:39:20 +0300

    add basic random character

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
 
-import MarvelService from './services/MarvelService';
-
-const marvelService = new MarvelService();
-
-marvelService
-  .getAllCharacters()
-  .then(res => res.data.results.forEach(item => console.log(item.name)));
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
