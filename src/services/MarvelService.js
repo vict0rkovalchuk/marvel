fa7fa29b9d1078a32990aa652930f8a92a96087f
Victor <vict02r05@gmail.com>
2023-04-12 21:01:26 +0300

    add data transformation

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,16 +12,28 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`
     );
+    return res.data.results.map(this._transformCharacter);
   };
 
-  getCharacter = id => {
-    return this.getResource(
+  getCharacter = async id => {
+    const res = await this.getResource(
       `${this._apiBase}characters/${id}?apikey=${this._apiKey}`
     );
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = char => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url
+    };
   };
 }
 
