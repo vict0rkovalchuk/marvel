a1eab049c37c0fe25270f8c76ab54d9352072585
Victor <vict02r05@gmail.com>
2023-04-13 17:51:21 +0300

    add characters listupdating

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,71 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from 'react';
 
-const CharList = () => {
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+
+import MarvelService from '../../services/MarvelService';
+
+import './charList.scss';
+
+class CharList extends Component {
+  state = {
+    characters: [],
+    loading: true,
+    error: false
+  };
+
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.updateCharacters();
+  }
+
+  onCharactersLoaded = characters => {
+    this.setState({ characters, loading: false });
+  };
+
+  onError = () => {
+    this.setState({ loading: false, error: true });
+  };
+
+  updateCharacters = () => {
+    this.marvelService
+      .getAllCharacters()
+      .then(this.onCharactersLoaded)
+      .catch(this.onError);
+  };
+
+  render() {
+    const { characters, loading, error } = this.state;
+
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? (
+      <View characters={characters} />
+    ) : null;
+
     return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+      <div className="char__list">
+        {errorMessage}
+        {spinner}
+        {content}
+        <button className="button button__main button__long">
+          <div className="inner">load more</div>
+        </button>
+      </div>
+    );
+  }
 }
+
+const View = ({ characters }) => {
+  const charactersList = characters.map(({ name, thumbnail, id }) => (
+    <li data-id={id} key={id} className="char__item">
+      <img src={thumbnail} alt={name} />
+      <div className="char__name">{name}</div>
+    </li>
+  ));
+
+  return <ul className="char__grid">{charactersList}</ul>;
+};
 
 export default CharList;
